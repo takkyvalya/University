572952101eb8618ea1c66e3588b9b6495da901a1
takkyvalya <65748453+takkyvalya@users.noreply.github.com>
2022-11-19 10:33:52 +0200

    Add files via upload

---
+++ Func.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+
+using namespace std;
+
+namespace lab {
+	int Number(int M[1000]);
+	int* Addition(int A[1000], int n, int B[1000], int m);
+	int FinalNumber(int Sub[1000], int n, int m);
+	int* Multiplication(int A[1000], int n, int B[1000], int m);
+	int* MultiplicationOfNumber(int B[1000], int n, int A);
+}
