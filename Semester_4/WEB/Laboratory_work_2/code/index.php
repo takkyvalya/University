71c2ad07af181e340d27790f2b513e622b828fff
vtakky <65748453+takkyvalya@users.noreply.github.com>
2024-03-04 21:19:03 +0200

    Update index.php

--- Semester_4/WEB/Laboratory_work_2/code/index.php
+++ Semester_4/WEB/Laboratory_work_2/code/index.php
@@ -10,5 +10,22 @@ $order .= " very spicy";
 
 echo "$order <br>";
 
+$int1 = 1e4;
+$int2 = 5279488;
+$float = 4.424;
+$No12 = 10;
+$no12 = 2;
+$HereNo12 = $No12 + $no12;
+
+echo "$int1<br>";
+echo "$int2<br>";
+echo "$float<br>";
+echo "Здесь нет 12: {$HereNo12}<br>";
+
+$lastMonth = 1187.23;
+$currentMonth = 1089.98;
+$difference = $lastMonth - $currentMonth;
+echo "В этом месяце я потратила на $difference долларов больше чем в предыдущем <br>";
+
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
