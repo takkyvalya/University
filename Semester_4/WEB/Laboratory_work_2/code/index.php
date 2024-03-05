d599abb6606aa265e59fcc6de901d56f85d4b19d
vtakky <65748453+takkyvalya@users.noreply.github.com>
2024-03-05 17:17:21 +0200

    Update index.php (complete task 14)

--- Semester_4/WEB/Laboratory_work_2/code/index.php
+++ Semester_4/WEB/Laboratory_work_2/code/index.php
@@ -45,5 +45,89 @@ $answer /= 2;
 $answer -= $my_num;
 echo "Переменная answer = $answer, задание выполнено верно<br>";
 
+$a=10;
+$b=3;
+$remainder = $a % $b;
+echo "Остаток от деления $a на $b = $remainder <br>";
+if ($remainder == 0) {
+    echo "Делится:";
+    $ab = $a / $b;
+    echo " $a / $b=$ab<br>";
+} else {
+    echo "Делится с остатком $remainder<br>";
+}
+
+$st = pow(2, 10);
+$sqrt = sqrt(245);
+$mas = [4, 2, 5, 19, 13, 0, 10];
+$sum = 0;
+foreach ($mas as $i) {
+    $sum += pow($i,2);
+}
+$sum = sqrt($sum);
+echo "2^10 = $st; квадратный корень из 245 = $sqrt<br>";
+echo "Корень из суммы квадратов элементов массива = $sum<br>";
+
+$sqrt1 = round(sqrt(379));
+$sqrt2 = round(sqrt(379), 1);
+$sqrt3 = round(sqrt(379), 2);
+$sqrt_of_587 = [];
+$sqrt_of_587['floor'] = floor(sqrt(579));
+$sqrt_of_587['ceil'] = ceil(sqrt(579));
+
+echo "квадратный корень из 379 = $sqrt1 = $sqrt2 = $sqrt3 <br>";
+echo "квадратный корень из 587 = {$sqrt_of_587['floor']}= {$sqrt_of_587['ceil']} <br>";
+
+$min = min(4, -2, 5, 19, -130, 0, 10);
+$max = max(4, -2, 5, 19, -130, 0, 10);
+echo "Минимальное: $min; максимальное: $max<br>";
+
+$random = rand(1, 100);
+echo "Случайно число от 1 до 100: $random <br>";
+
+$mas_for_random = [];
+foreach (range(0, 9) as $i) {
+    $mas_for_random[] = rand(1, 10000);
+    echo "$mas_for_random[$i] ";
+}
+echo "<br>";
+
+$module1 = abs($a - $b);
+$module2 = abs(($a = 24) - ($b = 12));
+echo "Работа с модулем:$module1; $module2 <br>";
+
+$mas = [1, 2, -1, -2, 3, -3];
+foreach (range(0, count($mas) - 1) as $i) {
+        $mas[$i] = abs($mas[$i]);
+        echo "$mas[$i] ";
+}
+echo "<br>";
+
+
+$mas_for_dividers = [];
+$number = 30;
+echo "Делители $number:";
+foreach (range(1, $number) as $i) {
+    if ($number % $i== 0) {
+        $mas_for_dividers[] = $i;
+    }
+}
+foreach ($mas_for_dividers as $i) {
+    echo $i . " ";
+}
+echo "<br>";
+
+$sumElementsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$count = 0;
+$sum = 0;
+foreach ($sumElementsArray as $i) {
+    $sum += $i;
+    $count++;
+    if ($sum > 10) {
+        break;
+    }
+}
+echo "Надо сложить первые $count чисел <br>";
+
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
